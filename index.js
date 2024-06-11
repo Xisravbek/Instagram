cb6db6054f2f2f9a56ac4c2c16f133ec809614bf
Usmonjonjon <usmonjona007@gmail.com>
2024-06-11 19:00:06 +0500

    Men tug'ildim

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ const app = express();
 app.get('/', (req, res ) => {
     res.send("Home")
     
+
+
+
+
+
 })
 
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connect(MONGO_URL, {}).then(() => {
     app.listen(PORT , console.log(`Server started in ${PORT} PORT `));
-}).catch(err => {
+}).catch(err => { 
     console.log(err);
 })
 
