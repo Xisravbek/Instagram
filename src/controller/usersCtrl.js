8d05ab95e30f9442ed14e50c531754da54847855
Xojiakbar1A <xojiakbarabroriy10@gmail.com>
2024-06-13 16:28:19 +0500

    signupda brhil ismli royhattan otomidgan qldm

--- src/controller/usersCtrl.js
+++ src/controller/usersCtrl.js
@@ -7,14 +7,19 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 
 const userCtrl = {
     SignUp: async (req,res) => {
-        const {email} = req.body
+        const {email,userName} = req.body
         try {
-            const Useritekshr = await Users.findOne({email})
+            const users = await Users.findOne({email})
 
-            if(Useritekshr){
+            const username = await Users.findOne({userName})
+
+            if(users){
                 return res.status(400).json({message: "Bu emailli foydalanuvchi mavjud!"})
             }
 
+            if(userName){
+                return res.status(400).json({message: "Bu nomli foydalanuvchi mavjud!"})
+            }
             const paswordhash = await bcrypt.hash(req.body.password,10)
 
             req.body.password = paswordhash
