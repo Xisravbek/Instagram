afcdb25143bec067300e5ec30c158fb451e1cb9c
Xojiakbar1A <xojiakbarabroriy10@gmail.com>
2024-06-13 12:21:04 +0500

    Login,hammasini korish,updet va deletti qdlm

--- src/controller/usersCtrl.js
+++ src/controller/usersCtrl.js
@@ -33,6 +33,101 @@ const userCtrl = {
             res.status(503).send({message: error.message})
 
         }
+    },
+    Login: async (req,res) => {
+        try {
+            const {email,password}= req.body
+            
+            const user = await Users.findOne({email})
+
+            if(!user){
+                return res.status(404).json({message: "Email yoki parol notogri"})
+            }
+
+            const verifiy = await bcrypt.compare(password,user.password)
+
+            
+            if(!verifiy){
+                return res.status(400).json({massage: "Email yoki parol notogri"})
+            }
+   
+            delete user._doc.password
+            const token = JWT.sign(user._doc, JWT_SECRET_KEY,{expiresIn:'2h'})
+
+            res.status(201).send({massage: "Login Success" , user, token})
+
+        } catch (error) {
+            res.status(503).send({message: error.message})
+        }
+    },
+    getAllUsers: async (req,res) => {
+        try {
+            let user = await Users.find()
+            if(!user){
+                return res.status(400).json({massage: "Users not found"})
+            }
+            user = user.map(users => {
+                const {password,...otherDetails}= users._doc
+                return otherDetails
+            })
+
+            res.status(200).send({message:'All Users',user})
+        } catch (error) {
+            res.status(503).send({message: error.message})
+            
+        }
+    },
+    updateUser: async (req,res) => {
+        try {
+            const {id}= req.params
+            if(id === req.user._id|| req.isAdmin){
+                const user = await Users.findById(id)
+
+                if(!user){
+                    return  res.status(404).json({massage:'User not found'})
+                }
+
+                if(req.body.password && req.body.password != ""){
+                    const hashedPass = await bcrypt.hash(req.body.password,10)
+                    req.body.password = hashedPass
+                }else{
+                    delete req.body.password
+                }
+
+                const updateUser =await Users.findByIdAndUpdate(id,req.body,{new:true})
+
+                return res.status(200).json({massage:"User update seccessfully",user:updateUser})
+                
+            }
+
+        } catch (error) {
+            res.status(503).send({message: error.message})
+        }
+
+    },
+    deleteUser: async (req,res) => {
+        try {
+            const {id} = req.params
+            if(id === req.user._id|| req.isAdmin){
+                const user = await Users.findById(id)
+
+                if(!user){
+                    return res.status(404).json({massage:'User not found'})
+                }
+
+                if(req.isAdmin){
+                    const deleteUser = await Users.findByIdAndDelete(id)
+
+                   return res.status(200).send({message:'deleted user succesfuly'})
+                }
+                const deleteUser = await Users.findByIdAndDelete(id)
+
+                return res.status(200).send({message:'deleted user succesfuly',deleteUser})
+            }
+        } catch (error) {
+            res.status(503).send({message: error.message})
+        }
     }
+
 }
 module.exports = userCtrl
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,6 +4,12 @@ const router = express.Router()
 
 const usersCtrl = require('../controller/usersCtrl')
 
+const authMiddlware = require('../authMiddleware/authMiddleware')
+
 router.post('/signup',usersCtrl.SignUp)
+router.post('/login',usersCtrl.Login)
+router.get('/',usersCtrl.getAllUsers)
+router.put('/:id',authMiddlware,usersCtrl.updateUser)
+router.delete('/:id',authMiddlware,usersCtrl.deleteUser)
 
 module.exports = router;
